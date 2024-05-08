c543ed33e750d15a2eaef2d3cc5d1d69107058f9
EmanuelBoscacci <boscacciemanuel@gmail.com>
2024-05-08 19:07:01 -0300

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,12 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+/*namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double numero;
+            double suma = 0;
+            Console.WriteLine("ingrese valores positivos,para finalizar ingrese 0");
+            while (true)
+            {
+                Console.Write("Ingrese un número: ");
+                {
+                    if (double.TryParse(Console.ReadLine()?.Replace(".", "."), out numero)) ;
+                }
+                if (numero == 0)
+                { 
+                break;
+                }
+                if (numero > 0)
+                {
+                    suma += numero;
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo valores positivos.");
+                    continue;
+                }
+            }
+
+            Console.WriteLine("La suma de los números ingresados es: " + suma);
         }
     }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
